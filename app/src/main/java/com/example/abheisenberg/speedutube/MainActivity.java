581dff12b8a01505a6b6fbe17a1f39580f9b1aae
abhisenberg <ab96sh@gmail.com>
2017-09-06 22:04:48 +0530

    starting fresh

--- app/src/main/java/com/example/abheisenberg/speedutube/MainActivity.java
+++ app/src/main/java/com/example/abheisenberg/speedutube/MainActivity.java
@@ -41,15 +41,12 @@ import at.huber.youtubeExtractor.VideoMeta;
 import at.huber.youtubeExtractor.YouTubeExtractor;
 import at.huber.youtubeExtractor.YtFile;
 
-public class MainActivity extends AppCompatActivity implements  VideoRendererEventListener{
+public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "MA";
     private  SimpleExoPlayer player;
+    public static String usableUrl;
     public static final String rawUrl = "https://www.youtube.com/watch?v=4Aa9GwWaRv0";
-
-    /*
-        LifeCycle methods
-     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,135 +54,9 @@ public class MainActivity extends AppCompatActivity implements  VideoRendererEve
         setContentView(R.layout.activity_main);
 
 
-        // Create a default track selector
-        Handler mainHandler = new Handler();
-        BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
-        TrackSelection.Factory videoTrackSelectionFactory =
-                new AdaptiveTrackSelection.Factory(bandwidthMeter);
-        TrackSelector trackSelector =
-                new DefaultTrackSelector(videoTrackSelectionFactory);
-
-        //Create a load control
-        LoadControl loadControl = new DefaultLoadControl();
-
-        //Creating the player
-        player =
-                ExoPlayerFactory.newSimpleInstance(this, trackSelector);
-        SimpleExoPlayerView playerView = new SimpleExoPlayerView(this);
-        playerView = (SimpleExoPlayerView) findViewById(R.id.exoPl_ma);
-
-        //Set media conroller
-        playerView.setUseController(true);
-        playerView.requestFocus();
-
-        //Build the player to the view
-        playerView.setPlayer(player);
-
-        YTLinkExtractor ytLinkExtractor = new YTLinkExtractor(rawUrl, this);
-        String finalurl = ytLinkExtractor.getYoutubeDownloadUrl();
-        Log.d(TAG, "finalurl "+finalurl);
-        Uri vidURL = Uri.parse(finalurl);
-
-        DefaultBandwidthMeter bandwidthMeterA = new DefaultBandwidthMeter();
-        DefaultDataSourceFactory dataSourceFactory =
-                new DefaultDataSourceFactory(
-                        this,
-                        Util.getUserAgent(this, "speedutube"),
-                        bandwidthMeterA
-                );
-
-        ExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
-
-        //For video streaming
-        MediaSource videoSource = new HlsMediaSource(vidURL, dataSourceFactory, 1, null, null);
-        final LoopingMediaSource loopingMediaSource = new LoopingMediaSource(videoSource);
-
-        player.prepare(loopingMediaSource);
-
-        player.addListener(new ExoPlayer.EventListener() {
-            @Override
-            public void onTimelineChanged(Timeline timeline, Object manifest) {
-                Log.d(TAG, "onTimelineChanged: ");
-            }
-
-            @Override
-            public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
-                Log.d(TAG, "onTracksChanged: ");
-            }
-
-            @Override
-            public void onLoadingChanged(boolean isLoading) {
-                Log.d(TAG, "onLoadingChanged: ");
-            }
-
-            @Override
-            public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
-                Log.d(TAG, "onPlayerStateChanged: ");
-            }
-
-            @Override
-            public void onRepeatModeChanged(int repeatMode) {
-                Log.d(TAG, "onRepeatModeChanged: ");
-            }
-
-            @Override
-            public void onPlayerError(ExoPlaybackException error) {
-                Log.d(TAG, "onPlayerError: ");
-                player.stop();
-//                player.prepare(loopingMediaSource);
-//                player.setPlayWhenReady(true);
-            }
-
-            @Override
-            public void onPositionDiscontinuity() {
-                Log.d(TAG, "onPositionDiscontinuity: ");
-            }
-
-            @Override
-            public void onPlaybackParametersChanged(PlaybackParameters playbackParameters) {
-                Log.d(TAG, "onPlaybackParametersChanged: ");
-            }
-        });
-
-        player.setPlayWhenReady(true);
-        player.setVideoDebugListener(this);
 
     }
 
-    @Override
-    public void onVideoEnabled(DecoderCounters counters) {
-
-    }
-
-    @Override
-    public void onVideoDecoderInitialized(String decoderName, long initializedTimestampMs, long initializationDurationMs) {
-
-    }
-
-    @Override
-    public void onVideoInputFormatChanged(Format format) {
-
-    }
-
-    @Override
-    public void onDroppedFrames(int count, long elapsedMs) {
-
-    }
-
-    @Override
-    public void onVideoSizeChanged(int width, int height, int unappliedRotationDegrees, float pixelWidthHeightRatio) {
-        Log.d(TAG, "onVideoSizeChanged: ");
-    }
-
-    @Override
-    public void onRenderedFirstFrame(Surface surface) {
-
-    }
-
-    @Override
-    public void onVideoDisabled(DecoderCounters counters) {
-
-    }
 
     @Override
     protected void onDestroy() {
